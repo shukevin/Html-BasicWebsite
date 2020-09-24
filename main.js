c8e2692a5bfdc4cbe4bb7aa25c530d1c8cda9583
Kevin Shu <61431892+shukevin@users.noreply.github.com>
2020-09-23 23:45:14 -0400

    changed sizing values to be relative for better resizing display

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ const sidePanel = document.getElementById("panel");
 const sidePanelButton = document.getElementById("side-panel-button")
 
 function openPanel(){
-    if(sidePanel.style.width == 235 + 'px'){
+    if(sidePanel.style.width == 15 + 'vw'){
         sidePanel.style.width = 0;
         sidePanelButton.style.color = 'whitesmoke';
         
         
     } else{
-        sidePanel.style.width = 235 + 'px';
+        sidePanel.style.width = 15 + 'vw';
         sidePanelButton.style.color = '#259a7a';
 
     }
