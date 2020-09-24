89cc3c6aa8eba7c60ace7f2365b0e986d2f4e5ab
Kevin Shu <61431892+shukevin@users.noreply.github.com>
2020-09-24 00:30:56 -0400

    Added screen responsiveness to side-panel on mobile/small scale devices

--- main.js
+++ main.js
@@ -2,15 +2,25 @@ const sidePanel = document.getElementById("panel");
 const sidePanelButton = document.getElementById("side-panel-button")
 
 function openPanel(){
-    if(sidePanel.style.width == 15 + 'vw'){
-        sidePanel.style.width = 0;
-        sidePanelButton.style.color = 'whitesmoke';
-        
-        
-    } else{
-        sidePanel.style.width = 15 + 'vw';
-        sidePanelButton.style.color = '#259a7a';
+    if (window.matchMedia('screen and (max-width: 768px)').matches) {
+        if(sidePanel.style.width == 100 + '%'){
+            sidePanel.style.width = 0;
+            sidePanelButton.style.color = 'whitesmoke';
+        } else{
+            sidePanel.style.width = 100 + '%';
+            sidePanelButton.style.color = '#259a7a';
+        }
 
+    } else {
+        if(sidePanel.style.width == 15 + 'vw'){
+            sidePanel.style.width = 0;
+            sidePanelButton.style.color = 'whitesmoke';
+        } else{
+            sidePanel.style.width = 15 + 'vw';
+            sidePanelButton.style.color = '#259a7a';
+        }
     }
+
+
     
 }
